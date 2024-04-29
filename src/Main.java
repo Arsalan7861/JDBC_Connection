5d9c2b00c79caf81c9165c600d70afbdd95bde52
Arsalan Khroush <arsalan_khroush@yahoo.com>
2024-04-29 22:33:47 +0300

    DAO

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.sql.*;
 /*
 1. import --> java.sql
-2. load and register the driver --> com.mysql.jdbc.Driver
+2. load and register the driver --> com.mysql.cj.jdbc.Driver
 3. Create Connection --> Connection
 4. Create a statement --> Statement
 5. execute the query
@@ -13,16 +13,33 @@ public class Main {
         String url = "jdbc:mysql://localhost:3307/arsalankhroush";
         String name = "root";
         String pass = "arsalankhroush123@";
-        String query = "select * from ogrenci";
+        int id = 8;
+        String fName = "";
+        String lName = "";
+        int age = 56;
+//        String query = "select * from ogrenci";
+        //String query = "insert into ogrenci values (7, 'Ahmad', 'Ali', 67)";
+        String query = "insert into ogrenci values (?, ?, ?, ?)";
 
         Class.forName("com.mysql.cj.jdbc.Driver");
         Connection con = DriverManager.getConnection(url, name, pass);
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery(query);
-        while (rs.next()) {
-            System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
-        }
+//        Statement stmt = con.createStatement();
+        PreparedStatement stmt = con.prepareStatement(query);
+        stmt.setInt(1, id);
+        stmt.setString(2, fName);
+        stmt.setString(3, lName);
+        stmt.setInt(4, age);
+//        ResultSet rs = stmt.executeQuery(query);//DQL
+        int count = stmt.executeUpdate();//DML
+        System.out.println(count + "row/s effected");
+//        while (rs.next()) {//Printing data
+//            System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
+//        }
         stmt.close();
         con.close();
     }
 }
+//DDL : Data definition language : used whenever a structure of the database is changed.
+//DML : Change the value of the data
+//DQL : Data Query Language
+//TCL : Transaction Control Language
---
+++ src/Main2.java
@@ -0,0 +1,87 @@
+import java.sql.*;
+
+//Editing data in database using DAO(Data Access Object) structure.
+public class Main2 {
+    public static void main(String[] args) {
+        StudentDAO dao = new StudentDAO();
+//        Student s1 = dao.getStudent(3);//Calling with id
+//        System.out.println("First Name: " + s1.fName);
+//        System.out.println("Last Name: " + s1.lName);
+//        System.out.println("Age: " + s1.age);
+//        Student student = new Student();
+//        student.id = 9;
+//        student.fName = "Muhammed";
+//        student.lName = "Ali";
+//        student.age = 25;
+//        dao.connect();
+//        dao.addStudent(student);
+        dao.connect();
+        dao.removeStudent(9);
+
+    }
+}
+
+class StudentDAO{
+    String url = "jdbc:mysql://localhost:3307/arsalankhroush";
+    String name = "root";
+    String pass = "arsalankhroush123@";
+    Connection con = null;
+    PreparedStatement pst = null;
+    Statement stmt = null;
+    public void connect(){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, name, pass);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public Student getStudent(int id){
+        try {
+            String query = "select * from ogrenci where id = " + id;
+            Student s = new Student();
+            s.id = id;
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, name, pass);
+            stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            rs.next();
+            s.fName = rs.getString(2);
+            s.lName = rs.getString(3);
+            s.age = rs.getInt(4);
+            return s;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
+    public void addStudent(Student s){
+        try {
+            String query = "insert into ogrenci values (?,?,?,?)";
+            pst = con.prepareStatement(query);
+            pst.setInt(1, s.id);
+            pst.setString(2, s.fName);
+            pst.setString(3, s.lName);
+            pst.setInt(4, s.age);
+            pst.executeUpdate();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public void removeStudent(int id){
+        try {
+            String query = "delete from ogrenci where id = " + id;
+            stmt = con.createStatement();
+            stmt.executeUpdate(query);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+class Student{
+    int id;
+    String fName;
+    String lName;
+    int age;
+}
